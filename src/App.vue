9fa237048a4f79de0305a0686a2d92b812a894cc
JesseSousa <rhadamesjesse14rj@gmail.com>
2022-03-17 17:43:01 -0300

    import and render right components

--- src/App.vue
+++ src/App.vue
@@ -2,5 +2,27 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import Leader from './components/Leader.vue'
-import Wrappers from './components/Wrapper.vue'
+import Wrapper from './components/Wrapper.vue'
 </script>
+
+<template>
+  <wrapper>
+    <Leader />
+  </wrapper>
+</template>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;0,700;1,300;1,400&display=swap");
+
+/* Reset */
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: "Lato", serif;
+  /* font-family: Helvetica; */
+}
+</style>
