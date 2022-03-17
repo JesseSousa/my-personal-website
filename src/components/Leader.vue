4d036ad9079056f70593dd4fc9d9a1d05b0a5f6a
JesseSousa <rhadamesjesse14rj@gmail.com>
2022-03-17 17:42:24 -0300

    create leader component

---
+++ src/components/Leader.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Jessé Sousa</h1>
+    <p>Full-Stack Developer</p>
+    <div class="icons">
+      <font-awesome-icon icon="fa-brands fa-github" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+div {
+  color: #fff;
+  text-align: center;
+}
+
+h1 {
+  font-size: 4rem;
+  font-weight: 400;
+  color: #ffd60a;
+  text-shadow: 6px 6px 8px #222;
+}
+
+p {
+  font-size: 1.5rem;
+}
+</style>
