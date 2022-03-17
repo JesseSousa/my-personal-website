dc79a91202510b0c5ffc70710a129fef6143767f
JesseSousa <rhadamesjesse14rj@gmail.com>
2022-03-17 16:33:47 -0300

    first commit

---
+++ src/components/Wrapper.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="wrapper">
+
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+  .wrapper {
+    width: 100vh;
+    height: 100vh;
+    background-image: url(../assets/background.jpg)
+  }
+</style>
