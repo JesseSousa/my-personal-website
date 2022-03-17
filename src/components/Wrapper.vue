e97ee77d94057379386b27f068e4b3cb5d71b404
JesseSousa <rhadamesjesse14rj@gmail.com>
2022-03-17 17:42:39 -0300

    create wrapper component

--- src/components/Wrapper.vue
+++ src/components/Wrapper.vue
@@ -1,16 +1,44 @@
 <template>
-  <div class="wrapper">
-
+  <div class="wrapper"></div>
+  <div class="overlay"></div>
+  <div class="container">
+    <div class="leader">
+      <slot></slot>
+    </div>
   </div>
 </template>
 
 <script setup>
 </script>
 
-<style lang="scss" scoped>
-  .wrapper {
-    width: 100vh;
-    height: 100vh;
-    background-image: url(../assets/background.jpg)
-  }
+<style scoped>
+.wrapper {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-image: url(../assets/background.jpg);
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+
+.overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background: rgba(0, 29, 61, 0.45);
+}
+
+.container {
+  position: absolute;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
 </style>
